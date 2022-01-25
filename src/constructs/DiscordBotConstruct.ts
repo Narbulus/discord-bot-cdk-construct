9875cffe1f8e237ced8d52b3e74d631658d2bcbf
Narbulus <s.hughes.anderson@gmail.com>
2022-01-24 18:56:41 -0800

    Update to CDK 2.0

--- src/constructs/DiscordBotConstruct.ts
+++ src/constructs/DiscordBotConstruct.ts
@@ -1,9 +1,15 @@
-import {Function, Runtime} from '@aws-cdk/aws-lambda';
-import {Cors, LambdaIntegration, RequestValidator, RestApi} from '@aws-cdk/aws-apigateway';
-import {NodejsFunction} from '@aws-cdk/aws-lambda-nodejs';
-import {Construct, Duration} from '@aws-cdk/core';
-import * as path from 'path';
-import {Secret} from '@aws-cdk/aws-secretsmanager';
+import { Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import {
+  Cors,
+  LambdaIntegration,
+  RequestValidator,
+  RestApi,
+} from "aws-cdk-lib/aws-apigateway";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Duration } from "aws-cdk-lib/core";
+import { Secret } from "aws-cdk-lib/aws-secretsmanager";
+import { Construct } from "constructs";
+import * as path from "path";
 
 /**
  * The properties required for the Discord Bot construct. Specifically
@@ -32,13 +38,13 @@ export class DiscordBotConstruct extends Construct {
     super(scope, id);
 
     // Create our Secrets for our Discord APIs.
-    this.discordAPISecrets = new Secret(this, 'discord-bot-api-key');
+    this.discordAPISecrets = new Secret(this, "discord-bot-api-key");
 
     // Create the Lambda for handling Interactions from our Discord bot.
-    const discordBotLambda = new NodejsFunction(this, 'discord-bot-lambda', {
+    const discordBotLambda = new NodejsFunction(this, "discord-bot-lambda", {
       runtime: Runtime.NODEJS_14_X,
-      entry: path.join(__dirname, '../functions/DiscordBotFunction.js'),
-      handler: 'handler',
+      entry: path.join(__dirname, "../functions/DiscordBotFunction.js"),
+      handler: "handler",
       environment: {
         DISCORD_BOT_API_KEY_NAME: this.discordAPISecrets.secretName,
         COMMAND_LAMBDA_ARN: props.commandsLambdaFunction.functionArn,
@@ -46,67 +52,75 @@ export class DiscordBotConstruct extends Construct {
       timeout: Duration.seconds(3),
     });
     props.commandsLambdaFunction.addEnvironment(
-        'DISCORD_BOT_API_KEY_NAME', this.discordAPISecrets.secretName);
+      "DISCORD_BOT_API_KEY_NAME",
+      this.discordAPISecrets.secretName
+    );
 
     this.discordAPISecrets.grantRead(discordBotLambda);
     this.discordAPISecrets.grantRead(props.commandsLambdaFunction);
     props.commandsLambdaFunction.grantInvoke(discordBotLambda);
 
     // Create our API Gateway
-    const discordBotAPI = new RestApi(this, 'discord-bot-api', {
+    const discordBotAPI = new RestApi(this, "discord-bot-api", {
       defaultCorsPreflightOptions: {
         allowOrigins: Cors.ALL_ORIGINS,
       },
     });
-    const discordBotAPIValidator = new RequestValidator(this, 'discord-bot-api-validator', {
-      restApi: discordBotAPI,
-      validateRequestBody: true,
-      validateRequestParameters: true,
-    });
+    const discordBotAPIValidator = new RequestValidator(
+      this,
+      "discord-bot-api-validator",
+      {
+        restApi: discordBotAPI,
+        validateRequestBody: true,
+        validateRequestParameters: true,
+      }
+    );
 
     // User authentication endpoint configuration
-    const discordBotEventItems = discordBotAPI.root.addResource('event', {
+    const discordBotEventItems = discordBotAPI.root.addResource("event", {
       defaultCorsPreflightOptions: {
-        allowOrigins: [
-          '*',
-        ],
+        allowOrigins: ["*"],
       },
     });
 
     // Transform our requests and responses as appropriate.
-    const discordBotIntegration: LambdaIntegration = new LambdaIntegration(discordBotLambda, {
-      proxy: false,
-      requestTemplates: {
-        'application/json': '{\r\n\
+    const discordBotIntegration: LambdaIntegration = new LambdaIntegration(
+      discordBotLambda,
+      {
+        proxy: false,
+        requestTemplates: {
+          "application/json":
+            '{\r\n\
               "timestamp": "$input.params(\'x-signature-timestamp\')",\r\n\
               "signature": "$input.params(\'x-signature-ed25519\')",\r\n\
               "jsonBody" : $input.json(\'$\')\r\n\
             }',
-      },
-      integrationResponses: [
-        {
-          statusCode: '200',
         },
-        {
-          statusCode: '401',
-          selectionPattern: '.*[UNAUTHORIZED].*',
-          responseTemplates: {
-            'application/json': 'invalid request signature',
+        integrationResponses: [
+          {
+            statusCode: "200",
           },
-        },
-      ],
-    });
+          {
+            statusCode: "401",
+            selectionPattern: ".*[UNAUTHORIZED].*",
+            responseTemplates: {
+              "application/json": "invalid request signature",
+            },
+          },
+        ],
+      }
+    );
 
     // Add a POST method for the Discord APIs.
-    discordBotEventItems.addMethod('POST', discordBotIntegration, {
+    discordBotEventItems.addMethod("POST", discordBotIntegration, {
       apiKeyRequired: false,
       requestValidator: discordBotAPIValidator,
       methodResponses: [
         {
-          statusCode: '200',
+          statusCode: "200",
         },
         {
-          statusCode: '401',
+          statusCode: "401",
         },
       ],
     });
